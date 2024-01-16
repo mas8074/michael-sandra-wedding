eda7c4da56271d9e223e12cbaa0ebbb6ebc33d58
Michael Samodurov <43452057+mas8074@users.noreply.github.com>
2024-01-15 19:58:10 -0500

    initial commit
    Created basic web page layout

---
+++ nav.js
@@ -0,0 +1,30 @@
+$(document).ready(function(){
+	// $("#navbar").append(`<div class="topnav">
+	// 					<h1 class="nav_title">RIT Player Piano</h1>
+	// 					<a href="index.html">Home</a>
+	// 					<a href="records.html">Records</a>
+	// 					<a href="contact.html">Contact</a>
+	// 					<a href="agenda.html">Agenda</a>
+	// 				</div>`);
+	$("#navbar").append(
+		`<nav class="navbar navbar-expand-sm">
+			<div class="container-fluid justify-content-center">
+			<!-- Links -->
+				<ul class="navbar-nav">
+					<li class="nav-item">
+						<a class="nav-link" href="index.html">Home</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="about_us.html">Our Story</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="registry.html">Registry</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="more_info.html">Wedding Details</a>
+					</li>
+				</ul>
+			</div>
+		
+		</nav>`);
+});
